f7d5a4e17e150ea91673db54addc57260a100769
NCTECH-FERNANDO88 <nctech.fernando@icloud.com>
2026-08-20 11:10:18 -0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string nome = "João";
-string sobrenome = "Silva";
-string nomeCompleto = $"{nome} {sobrenome}";
-string mensagem = $"Olá, {nomeCompleto}! Celular clonado com sucesso.";
-string mensagemFinal = Sou o seuu pior pesadelo, {nomeCompleto}. Prepare-se para enfrentar as consequências.
