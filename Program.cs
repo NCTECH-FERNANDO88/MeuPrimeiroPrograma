ae13f19811536caca97b63901606e05496539c4b
Fernando Costa <fernandsilva218@outlook.com>
2026-08-20 10:37:32 -0300

    Primeiro commit do projeto

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string nome = "João";
+string sobrenome = "Silva";
+string nomeCompleto = $"{nome} {sobrenome}";
+string mensagem = $"Olá, {nomeCompleto}! Celular clonado com sucesso.";
+string mensagemFinal = Sou o seuu pior pesadelo, {nomeCompleto}. Prepare-se para enfrentar as consequências.
